a144c95567b9ea29990ac19620122e863ebf486b
dlgksqls <dlgksqls7218@naver.com>
2026-08-20 16:03:00 +0900

    cicd 세팅

---
+++ src/main/java/com/example/toiletbatch/ToiletBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.toiletbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiletBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToiletBatchApplication.class, args);
+    }
+
+}
